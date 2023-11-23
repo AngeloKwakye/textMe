f1ca8b2412deea293c34d6dcc7df698d019150ab
D'Angelo <dangelokwakye@gmail.com>
2023-11-23 02:13:35 +0000

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'text-me-unique',
+    appId: '1:458630332260:web:d5106b2916fe6c88200471',
+    storageBucket: 'text-me-unique.appspot.com',
+    apiKey: 'AIzaSyA7lAfHjQ2Xwl4Uq7GmLlBjR2DwsvI4lPA',
+    authDomain: 'text-me-unique.firebaseapp.com',
+    messagingSenderId: '458630332260',
+  },
   production: true
 };
